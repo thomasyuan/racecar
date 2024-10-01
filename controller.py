5356e8313e155ac5d67134b9a2fb31362108b6f6
Thomas Yuan <thomas@cppcoder.net>
2024-10-01 17:06:34 -0400

    up

--- controller.py
+++ controller.py
@@ -3,6 +3,7 @@ from time import sleep
 command_registry = {}
 
 TURN_THRESHOLD = 40
+avoiding_obstacle = False
 
 def register_command(command_name, handler):
     command_registry[command_name] = handler
@@ -17,13 +18,20 @@ def handle_control_message(message):
 
 def handle_ultrasonic(distance):
     print(f"Distance: {distance} cm")
+    global avoiding_obstacle
     if (distance < TURN_THRESHOLD):
+        if avoiding_obstacle == True:
+            return
         print(f"Obstacle detected! {distance} cm")
         # Stop the car
         # handle_control_message({"command": "stop"})
         handle_control_message({"command": "turn", "direction":"right"})
+        avoiding_obstacle = True
     else:
+        if avoiding_obstacle == False:
+            return
         handle_control_message({"command": "turn", "direction":"center"})
+        avoiding_obstacle = False
 
 def handle_gyro(gyro_x, gyro_y, gyro_z):
     print(f"Gyro X: {gyro_x}, Gyro Y: {gyro_y}, Gyro Z: {gyro_z}")
